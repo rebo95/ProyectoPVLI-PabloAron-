814f8bc9a6823ef00baabbae8142cb40cadbd047
rebo95 <arebored@ucm.es>
2017-11-27 23:32:28 +0100

    Project first archives added

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var PlayScene = require('./play_scene.js');
+
+
+var BootScene = {
+  preload: function () {
+    // load here assets required for the loading screen
+    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+  },
+
+  create: function () {
+    this.game.state.start('preloader');
+  }
+};
+
+
+var PreloaderScene = {
+  preload: function () {
+    this.loadingBar = this.game.add.sprite(0, 240, 'preloader_bar');
+    this.loadingBar.anchor.setTo(0, 0.5);
+    this.load.setPreloadSprite(this.loadingBar);
+
+    // TODO: load here the assets for the game
+    this.game.load.image('fondo', 'images/background.jpg');
+    this.game.load.image('nave', 'images/photo.jpg');
+    this.game.load.image('laser', 'images/phaser.png')
+  },
+
+  create: function () {
+    this.game.state.start('play');
+  }
+};
+
+
+window.onload = function () {//esto se ejecuta cuando la ventana se carga 
+  //se produce una llamada en cadena a los diferentes métodos
+  // primero se añaden al entorno y se les asigna un tag con el 
+  // que llamarlos más fácilmente 
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+  
+  game.state.add('boot', BootScene);
+  game.state.add('preloader', PreloaderScene);
+  game.state.add('play', PlayScene);
+
+
+  game.state.start('boot');
+};
---
+++ src/js/play_scene.js
@@ -0,0 +1,77 @@
+'use strict';
+var nave;
+var lasers;
+var fireCounter= 0;
+var fireRate = 5;
+var fire = false;
+
+var PlayScene = {
+  
+  create: function () {
+
+    var fondo = this.game.add.sprite(
+      this.game.world.centerX, this.game.world.centerY, 'fondo');
+    fondo.anchor.setTo(0.5, 0.5);
+
+    this.game.physics.setBoundsToWorld();
+
+
+    nave = this.game.add.sprite(
+      this.game.world.centerX, this.game.world.centerY, 'nave');
+    nave.anchor.setTo(0.5, 0.5);
+    nave.scale.setTo(0.2,0.2);
+
+    lasers = this.game.add.group();
+    lasers.enableBody = true;
+    lasers.physicsBodyType = Phaser.Physics.ARCADE;
+
+
+  },
+
+
+
+update: function(){
+  if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+  {
+      nave.x -= 4;
+
+  }
+  else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+  {
+      nave.x += 4;
+  }
+
+  if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+  {
+      nave.y -= 4;
+  }
+  else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
+  {
+      nave.y += 4;
+  }
+
+  if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+
+  if(!fire){
+        var laser = lasers.create(nave.x,nave.y, 'laser');
+        laser.scale.setTo(0.1,0.1);
+        laser.body.velocity.x = 500;
+        fire = true ;
+  }
+  }
+  if (fire){
+    fireCounter++;
+    if(fireCounter>fireRate){
+      fire = false;
+      fireCounter = 0;
+    }
+  }       
+
+
+  
+
+},
+
+};
+
+module.exports = PlayScene;
