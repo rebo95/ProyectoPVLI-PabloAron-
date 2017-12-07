83f7205d33e14a928e0cf6868b94a55966924990
rebo95 <arebored@ucm.es>
2017-12-07 18:06:35 +0100

    tiles and camera movement implemented

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ var PlayScene = require('./play_scene.js');
 var BootScene = {
   preload: function () {
     // load here assets required for the loading screen
-    this.game.load.image('preloader_bar', 'images/preloader_bar.png');
+    
   },
 
   create: function () {
@@ -17,6 +17,11 @@ var BootScene = {
 
 var PreloaderScene = {
   preload: function () {
+
+
+    this.game.load.image('fondo', './images/background.jpg');
+    this.game.load.image('nave', './images/nave.png');
+    this.game.load.image('laser', './images/phaser.png');
 
     this.game.load.tilemap('tilemap', 'images/pableras.csv');
     this.game.load.image('tilespng', 'images/tiles.png');
